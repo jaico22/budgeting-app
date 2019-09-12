a66098e38977ebf8b35ddd76e2671dad0e23439b
Phillip Miller <phillip.miller@perficient.com>
2019-09-12 09:53:19 -0500

    activeBudgetId moved to router

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BudgetOverview from './Components/BudgetOverview/BudgetOverview'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
-        <BudgetOverview />
+      <Router>
+        <Route path="/:budgetId" render={(props) => <BudgetOverview {...props} />} />
+      </Router>
     </div>
   );
 }
